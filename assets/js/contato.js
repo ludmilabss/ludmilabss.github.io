806aed9ef1cc398bdc919c2a12e048e2fe3f103a
Ludmila Barbosa <80183918+ludmilabss@users.noreply.github.com>
2023-07-05 08:26:36 -0300

    Update contato.js

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -1,23 +1,13 @@
 document.querySelector('.exit-icon').addEventListener('click', function() {
-    window.location.href = '/index.html';
+  window.location.href = '/index.html';
 });
 
-// Obtém o formulário de contato pelo ID
 const formContato = document.querySelector('.contact-form');
 
-// Adiciona um ouvinte de evento de envio ao formulário
 formContato.addEventListener('submit', function(event) {
-  // Impede o comportamento padrão de envio do formulário
-    event.preventDefault();
+  event.preventDefault();
 
-  // Exibe a mensagem de confirmação como um popup
-    Swal.fire({
-        icon: 'success',
-        title: 'Mensagem enviada com sucesso!',
-        showConfirmButton: false,
-        timer: 2000
-    });
+  alert("Mensagem enviada com sucesso!");
 
-  // Limpa os campos do formulário
-    formContato.reset();
+  formContato.reset();
 });
